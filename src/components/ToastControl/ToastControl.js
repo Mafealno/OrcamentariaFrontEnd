fab96038519623bce3a8a09c1afc3dd2c1259204
Marcelo Fernando Alves Nogueira <mafealno@gmail.com>
2020-08-26 02:09:30 -0300

    Adicionado o metodo createPortal do ReactDOM para que o Toast seja renderizado no elemento <body/> para que fique por cima de modais quando uma estiver aberta. Adicionado o z-indez na class "estiloPadraoToast".

--- src/components/ToastControl/ToastControl.js
+++ src/components/ToastControl/ToastControl.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./ToastControl.css";
 import Toast from "react-bootstrap/Toast";
 import ToastHeader from "react-bootstrap/ToastHeader";
@@ -7,24 +8,29 @@ import ToastBody from "react-bootstrap/ToastBody";
 export default function ToastControl(props) {
   return (
     <>
-      <Toast
-        className={props.estiloToast ?? "estiloPadraoToast"}
-        onClose={() => props.closeToast(false)}
-        show={props.showToast ?? false}
-        delay={props.delayTost}
-        autohide={props.autoHideToast}
-      >
-        {props.hideToastHeader && (
-          <>
-            <ToastHeader className={props.estiloToastHeader}>
-              {props.conteudoHeader}
-            </ToastHeader>
-          </>
-        )}
-        <ToastBody className={props.estiloToastBody}>
-          {props.conteudoBody}
-        </ToastBody>
-      </Toast>
+      {ReactDOM.createPortal(
+        <>
+          <Toast
+            className={props.estiloToast ?? "estiloPadraoToast"}
+            onClose={() => props.closeToast(false)}
+            show={props.showToast ?? false}
+            delay={props.delayTost}
+            autohide={props.autoHideToast}
+          >
+            {props.hideToastHeader && (
+              <>
+                <ToastHeader className={props.estiloToastHeader}>
+                  {props.conteudoHeader}
+                </ToastHeader>
+              </>
+            )}
+            <ToastBody className={props.estiloToastBody}>
+              {props.conteudoBody}
+            </ToastBody>
+          </Toast>
+        </>,
+        document.getElementsByTagName("body")[0]
+      )}
     </>
   );
 }
