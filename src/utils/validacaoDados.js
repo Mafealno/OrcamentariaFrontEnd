993d20e431b19abb73b9e0a4e50553652399b0c1
Marcelo Fernando Alves Nogueira <mafealno@gmail.com>
2020-09-09 16:35:39 -0300

    criado nova pasta utils e novo arquivo validacaoDados e adicionado a funcao validarDados e a variavel dadosCampo.

---
+++ src/utils/validacaoDados.js
@@ -0,0 +1,36 @@
+export function validarDados(dadosValidacao) {
+  const objAux = Object.assign({}, dadosValidacao);
+
+  Object.keys(objAux).map((campo) => {
+    objAux[campo].valido = false;
+    if (objAux[campo].valor) {
+      if (objAux[campo].formato) {
+        if (objAux[campo].valor.match(objAux[campo].formato)) {
+          objAux[campo].valido = true;
+        } else {
+          objAux[campo].msgErro = "Formato inválido";
+        }
+      } else {
+        objAux[campo].valido = true;
+      }
+    } else {
+      if (!objAux[campo].requerido) {
+        objAux[campo].valor = objAux[campo].valorPadrao;
+        objAux[campo].valido = true;
+      } else {
+        objAux[campo].msgErro = "Preencha esta informação";
+      }
+    }
+  });
+
+  return objAux;
+}
+
+export const dadosCampo = {
+  valor: "",
+  valorPadrao: "",
+  requerido: false,
+  formato: "",
+  msgErro: "",
+  valido: false,
+};
