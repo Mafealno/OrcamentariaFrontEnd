313e567393ee028d561ecbc5b352286c1afcdd6e
Marcelo Fernando Alves Nogueira <mafealno@gmail.com>
2021-02-12 16:52:02 -0300

    Adicionado redenderização dos elementos trazidos do banco e nao mais testes

--- src/components/Orcamento/TotaisOrcamento/TotaisOrcamento.js
+++ src/components/Orcamento/TotaisOrcamento/TotaisOrcamento.js
@@ -1,34 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./TotaisOrcamento.css";
 import GraficoPizza from "../../GraficoPizza/GraficoPizza"
+import { connect } from "react-redux"
 
-function TotaisOrcamento() {
+function TotaisOrcamento(props) {
 
-    const teste = 
-    [
-        {
-            titulo: "aksnd",
-            valor: "244"
-        },
-        {
-            titulo: "qqrqweqweaksnd",
-            valor: "122"
-        },
-        {
-            titulo: "1svsds",
-            valor: "123"
-        },
-        {
-            titulo: "2343234",
-            valor: "50"
-        },
-    ]
+    const [valoresGrafico, setValoresGrafico] = useState([])
+
+    useEffect(() => {
+        if(props.totaisOrcamento.TOTAIS_ORCAMENTO_ID){
+            var objGrafico = []
+
+            if(props.totaisOrcamento.TOTAIS_ITENS){
+                objGrafico = [...objGrafico, {
+                    titulo: "Itens",
+                    valor: props.totaisOrcamento.TOTAIS_ITENS
+                }]
+            }
+            if(props.totaisOrcamento.TOTAIS_MAO_OBRA){
+                objGrafico = [...objGrafico, {
+                    titulo: "Mão de Obra",
+                    valor: props.totaisOrcamento.TOTAIS_MAO_OBRA
+                }]
+            }
+            if(props.totaisOrcamento.TOTAIS_EQUIPAMENTOS){
+                objGrafico = [...objGrafico, {
+                    titulo: "Equipamentos",
+                    valor: props.totaisOrcamento.TOTAIS_EQUIPAMENTOS
+                }]
+            }
+            if(props.totaisOrcamento.TOTAIS_CUSTOS){
+                objGrafico = [...objGrafico, {
+                    titulo: "Custos",
+                    valor: props.totaisOrcamento.TOTAIS_CUSTOS
+                }]
+            }
+
+            setValoresGrafico(objGrafico);
+        }else{
+            setValoresGrafico([])
+        }
+    }, [props.totaisOrcamento])
     
     return (
         <>
             <div id="containerTotaisOrcamento">
                 <div id="espaco-grafico">
-                    <GraficoPizza dadosGrafico={teste}/>
+                    <GraficoPizza dadosGrafico={valoresGrafico}/>
                 </div>
                 <div id="espaco-list-totais"></div>
                 <div id="espaco-totalizador"></div>
@@ -38,4 +56,11 @@ function TotaisOrcamento() {
     )
 }
 
-export default TotaisOrcamento
+const mapStateToProps = (state) => ({
+    linkBackEnd: state.backEnd.link,
+    totaisOrcamento: state.orcamento.totaisOrcamento
+  });
+  
+const mapDispatchToProps = (dispatch) => ({});
+  
+export default connect(mapStateToProps, mapDispatchToProps)(TotaisOrcamento);
